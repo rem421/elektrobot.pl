94489821e06442514fcbad52bc4028fc0abef19e
flipolipo <juroszekfilip123@gmail.com>
2024-02-15 22:21:03 +0100

    fix

--- Frontend/elektrobot-front/src/Pages/Kontakt.js
+++ Frontend/elektrobot-front/src/Pages/Kontakt.js
@@ -3,10 +3,13 @@ import MowerCard from "../Components/MowerCard";
 import "../Components/MowerCard.css";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import '@splidejs/react-splide/css';
+import ShopTypeSwitchButton from "../Components/ShopTypeSwitchButton";
+import "../Components/ShopTypeSwitchButton.css"
 
 const Kontakt = () => {
   return (
     <div>
+    
       <div className="test"></div>
       <Splide options={{ rewind: true, type: "loop", drag: "free", snap: true, perPage: "4", wheel: true, wheelSleep: "500"}}>
         <SplideSlide>
--- Frontend/elektrobot-front/src/Pages/Shop.js
+++ Frontend/elektrobot-front/src/Pages/Shop.js
@@ -1,8 +1,11 @@
 import React from "react";
+import "../Components/ShopTypeSwitchButton.css"
+import ShopTypeSwitchButton from "../Components/ShopTypeSwitchButton";
 const Shop = () =>{
     return(
         <div>
             <h2>Shop</h2>
+           <ShopTypeSwitchButton></ShopTypeSwitchButton>
         </div>
     )
 }
